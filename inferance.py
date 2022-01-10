5136054b6566aa82b9c02b18e21384a8bbcc5220
horizonsinzqs <73569961+horizonsinzqs@users.noreply.github.com>
2022-01-10 23:46:04 +0800

    change model to resnet101 change inferance

--- inferance.py
+++ inferance.py
@@ -1,45 +1,103 @@
-'''
-    请大家自行实现测试代码，注意提交格式
-'''
 import os
+import sys
+import numpy as np
+from PIL import Image
 
 import torch
+import torch.nn as nn
 
-from dataset import Food_LT
-from torch.autograd import Variable
+import torch.optim
+import torchvision
+import torchvision.datasets
 from torchvision import transforms
-from PIL import Image
-# from model import resnet34
-from resnet50 import ResNet50
-model = ResNet50()
-checkpoint = torch.load('/home/zhangzhengxin/PythonProject/MachineLearning/food_cls/ckpt/model_best.pth.tar')
-# start_epoch=checkpoint['epoch']
-model.load_state_dict(checkpoint['state_dict_model'])
-# model.load_state_dict()
+from torch.utils.data import Dataset
+import config as cfg
+import time
+import math
+from tqdm import tqdm
+from datetime import datetime
 
-normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# from model.model import resnet34
+# from model.densenet import densenet121, densenet161
+# from model.SWTDN import swtdn121
+
 transform_test = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
-    normalize,
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
-def predict_img(image):
-    image_tensor = transform_test(image).float()
-    image_tensor = image_tensor.unsqueeze_(0)
-    input = Variable(image_tensor).to('cpu')
-    output = model(input)
-    index = output.data.cpu().numpy().argmax()
-    return index
 
-root = '/home/zhangzhengxin/PythonProject/MachineLearning/food_cls/data/food/test/test/'
-f = open('submission.txt','w')
-f.write('Id, Expected\n')
-for filename in os.listdir(root):
-    image = Image.open(root+filename)
-    print(filename)
-    res = filename + ', ' + str(predict_img(image))+'\n'
+class Testset(Dataset):
+    num_classes = cfg.num_classes
 
-    f.write(res)
+    def __init__(self):
+        self.img_path = []
+        self.name = []
+        self.transform = transform_test
+        base = "data/food/test/"
+        files = os.listdir(os.path.join(cfg.root, base))  # 得到文件夹下的所有文件名称
+        for file in files:  # 遍历文件夹
+            self.img_path.append(os.path.join(os.path.join(cfg.root, base), file))
+            self.name.append(file)
+        print("load %d images in test folder" % (len(self.img_path)))
 
+    def __len__(self):
+        return len(self.img_path)
+
+    def __getitem__(self, index):
+        path = self.img_path[index]
+        with open(path, 'rb') as f:
+            sample = Image.open(f).convert('RGB')
+        if self.transform is not None:
+            sample = self.transform(sample)
+        return sample, self.name[index]
+
+
+def main():
+    # model = densenet161()
+    import torchvision as tv
+    # model = resnet34()
+    model = tv.models.resnet101()
+    in_feats = model.fc.in_features
+    out_feats = 101
+    model.fc = nn.Linear(in_feats, out_feats)
+
+    state_dict = torch.load(cfg.root + '/ckpt/model_best.pth.tar')
+    model.load_state_dict(state_dict['state_dict_model'])
+
+    if not torch.cuda.is_available():
+        # logger('Plz train on cuda !')
+        os._exit(0)
+
+    if cfg.gpu is not None:
+        print('Use cuda !')
+        torch.cuda.set_device(cfg.gpu)
+        model = model.cuda(cfg.gpu)
+
+    dataset = Testset()
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg.batch_size * 4, num_workers=4, pin_memory=True)
+    # switch to evaluate mode
+    model.eval()
+
+    class_num = torch.zeros(cfg.num_classes).cuda()
+    pred_class = np.array([])
+    # filename = 'result/result' + datetime.now().strftime("%m-%d-%H-%M") + '.txt'
+    filename = "submission.txt"
+    start = time.time()
+    with torch.no_grad():
+        with open(filename, 'w') as file_object:
+            file_object.write("Id,Expected\n")
+            for i, (images, name) in enumerate(tqdm(dataloader)):
+                images = images.cuda(cfg.gpu, non_blocking=True)
+                output = model(images)
+                _, predicted = output.max(1)
+                for i in range(len(predicted)):
+                    file_object.write(name[i] + "," + str(predicted[i].cpu().numpy()) + "\n")
+
+    print("\nFinish !  time:" + str(time.time() - start) + "s")
+
+
+if __name__ == '__main__':
+    main()
--- train.py
+++ train.py
@@ -17,16 +17,20 @@ import config as cfg
 from utils import adjust_learning_rate, save_checkpoint, train, validate, logger
 import torchvision as tv
 
+
 def main():
-    model = v.models.resnet50()
-    
+    model = tv.models.resnet101(pretrained=True)
+    in_feats = model.fc.in_features
+    out_feats = 101
+    model.fc = nn.Linear(in_feats, out_feats)
+
     if cfg.resume:
         ''' plz implement the resume code by ur self! '''
         pass
 
     print('log save at:' + cfg.log_path)
     logger('', init=True)
-    
+
     if not torch.cuda.is_available():
         logger('Plz train on cuda !')
         os._exit(0)
@@ -41,19 +45,20 @@ def main():
 
     train_loader = dataset.train_instance
     val_loader = dataset.eval
-    
+
     criterion = nn.CrossEntropyLoss().cuda(cfg.gpu)
     optimizer = torch.optim.SGD([{"params": model.parameters()}], cfg.lr,
-                                momentum=cfg.momentum,
-                                weight_decay=cfg.weight_decay)
-    
+                                 momentum=cfg.momentum,
+                                 weight_decay=cfg.weight_decay)
+    # optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=20, gamma=0.5)
     best_acc = 0
     for epoch in range(cfg.num_epochs):
-        logger('--'*10 + f'epoch: {epoch}' + '--'*10)
+        logger('--' * 10 + f'epoch: {epoch}' + '--' * 10)
         logger('Training start ...')
-        
+
         adjust_learning_rate(optimizer, epoch, cfg)
-        
+
         train(train_loader, model, criterion, optimizer, epoch)
         logger('Wait for validation ...')
         acc = validate(val_loader, model, criterion)
@@ -67,6 +72,7 @@ def main():
             'state_dict_model': model.state_dict(),
             'best_acc': best_acc,
         }, is_best, cfg.root)
+        scheduler.step()
 
     print('Finish !')
 
